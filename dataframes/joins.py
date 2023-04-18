be8b99b56b1b659c1840136668c09e19433ed76a
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-04-18 16:32:40 +0530

    changes in joins

--- dataframes/joins.py
+++ dataframes/joins.py
@@ -3,10 +3,10 @@ pd.concat Method
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv")
+foods = pd.read_csv("data/Restaurant - Foods.csv")
 week = pd.concat(objs=[week1, week2], ignore_index=True)
 week_alt = week1.append(other=week2, ignore_index=True)
 sales = pd.concat(objs=[week1, week2], keys=["Week1", "Week2"])
@@ -22,10 +22,10 @@ Inner Joins
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv")
+foods = pd.read_csv("data/Restaurant - Foods.csv")
 innerjoin1 = week1.merge(week2, how="inner", on="Customer ID", suffixes=[" - Week 1", " - Week 2"])
 innerjoin2 = week1.merge(week2, how="inner", on=["Customer ID", "Food ID"])
 print(innerjoin2)
@@ -35,10 +35,10 @@ Outer Joins
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv")
+foods = pd.read_csv("data/Restaurant - Foods.csv")
 outerjoin = week1.merge(week2, how="outer", on="Customer ID")
 merged = week1.merge(week2, how="outer", on="Customer ID", suffixes=[" - Week 1", " - Week 2"], indicator=True)
 condition = merged["_merge"].isin(["left_only", "right_only"])
@@ -50,10 +50,10 @@ Left Joins
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv")
+foods = pd.read_csv("data/Restaurant - Foods.csv")
 leftjoin = week1.merge(foods, how="left", on="Food ID", sort=True)
 print(leftjoin)
 
@@ -62,10 +62,10 @@ left_on and right_on parameter
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv")
+foods = pd.read_csv("data/Restaurant - Foods.csv")
 join = week2.merge(customers, how="left", left_on="Customer ID", right_on="ID", sort=True).drop("ID", axis="columns")
 print(join)
 
@@ -74,10 +74,10 @@ left_index and right_index parameter
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv", index_col="ID")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv", index_col="Food ID")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv", index_col="ID")
+foods = pd.read_csv("data/Restaurant - Foods.csv", index_col="Food ID")
 sales = week1.merge(customers, how="left", left_on="Customer ID", right_index=True)
 sales = sales.merge(foods, how="left", left_on="Food ID", right_index=True)
 print(sales)
@@ -87,11 +87,11 @@ join method
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv", index_col="ID")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv", index_col="Food ID")
-rating = pd.read_csv("pandas/data/Restaurant - Week 1 Satisfaction.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv", index_col="ID")
+foods = pd.read_csv("data/Restaurant - Foods.csv", index_col="Food ID")
+rating = pd.read_csv("data/Restaurant - Week 1 Satisfaction.csv")
 j1 = week1.join(rating)
 print(j1)
 
@@ -100,10 +100,10 @@ merge method
 """
 import pandas as pd
 
-week1 = pd.read_csv("pandas/data/Restaurant - Week 1 Sales.csv")
-week2 = pd.read_csv("pandas/data/Restaurant - Week 2 Sales.csv")
-customers = pd.read_csv("pandas/data/Restaurant - Customers.csv", index_col="ID")
-foods = pd.read_csv("pandas/data/Restaurant - Foods.csv", index_col="Food ID")
-rating = pd.read_csv("pandas/data/Restaurant - Week 1 Satisfaction.csv")
+week1 = pd.read_csv("data/Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("data/Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("data/Restaurant - Customers.csv", index_col="ID")
+foods = pd.read_csv("data/Restaurant - Foods.csv", index_col="Food ID")
+rating = pd.read_csv("data/Restaurant - Week 1 Satisfaction.csv")
 join = pd.merge(week1, customers, how="left", left_on="Customer ID", right_on="ID")
 print(join)
--- temp.py
+++ temp.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 fortune = pd.read_csv("data/fortune1000.csv", index_col="Rank")
 sectors = fortune.groupby("Sector")
-g1 = sectors.first()
-g3 = sectors.groups
-print(g3)
+df = pd.DataFrame(columns=fortune.columns)
+
+for sector, data in sectors:
+    print(data)
+    print("------------------------")
