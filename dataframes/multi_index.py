fa3ce0db87484f6d2aa371924e0a0985ddd7defb
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-04-20 12:55:21 +0530

    changes in multi index

--- dataframes/multi_index.py
+++ dataframes/multi_index.py
@@ -3,7 +3,7 @@ MultiIndex with set_index
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"])
 m1 = bigmac.set_index(keys=["Date", "Country"])
 m2 = bigmac.set_index(keys=["Country", "Date"])
 bigmac.set_index(keys=["Date", "Country"], inplace=True)
@@ -17,7 +17,7 @@ get_level_values method
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
 bigmac.sort_index(inplace=True)
 g1 = bigmac.index.get_level_values("Date")
 g2 = bigmac.index.get_level_values(0)
@@ -29,7 +29,7 @@ Change index level with set_names method
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
 bigmac.sort_index(inplace=True)
 s1 = bigmac.index.set_names(names=["Day", "Location"], inplace=True)
 s2 = bigmac.index.set_names(names="Date", level=0)
@@ -41,7 +41,7 @@ sort_index on MultiIndex DF
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
 s1 = bigmac.sort_index(ascending=True)
 s2 = bigmac.sort_index(ascending=False)
 s3 = bigmac.sort_index(ascending=[True, False])
@@ -54,7 +54,7 @@ loc on Multi Index DF
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
 bigmac.sort_index(inplace=True)
 l1 = bigmac.loc["2010-01-01", "Argentina"]
 l2 = bigmac.loc["2010-01-01", "Price in US Dollars"]
@@ -69,7 +69,7 @@ transpose Method
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
 bigmac.sort_index(inplace=True)
 t1 = bigmac.transpose()
 t2 = t1.loc[("Price in US Dollars",), ("2010-01-01", "Sri Lanka"):("2010-01-01", "Ukraine")]
@@ -80,7 +80,7 @@ swaplevel Method
 """
 import pandas as pd
 
-bigmac = pd.read_csv("pandas/data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
+bigmac = pd.read_csv("data/bigmac.csv", parse_dates=["Date"], index_col=["Date", "Country"])
 bigmac.sort_index(inplace=True)
 s1 = bigmac.swaplevel()
 s2 = bigmac.swaplevel("Date", "Country")
@@ -95,7 +95,7 @@ stack Method
 """
 import pandas as pd
 
-world = pd.read_csv("pandas/data/worldstats.csv", index_col=["country", "year"])
+world = pd.read_csv("data/worldstats.csv", index_col=["country", "year"])
 s1 = world.stack()
 s2 = world.stack().to_frame()
 print(s2)
@@ -105,7 +105,7 @@ unstack Method
 """
 import pandas as pd
 
-world = pd.read_csv("pandas/data/worldstats.csv", index_col=["country", "year"])
+world = pd.read_csv("data/worldstats.csv", index_col=["country", "year"])
 s = world.stack()
 us1 = s.unstack()
 us2 = s.unstack().unstack()
@@ -125,7 +125,7 @@ pivat Method
 """
 import pandas as pd
 
-sales = pd.read_csv("pandas/data/salesmen.csv", parse_dates=["Date"])
+sales = pd.read_csv("data/salesmen.csv", parse_dates=["Date"])
 sales["Salesman"] = sales["Salesman"].astype("category")
 p1 = sales.pivot(index="Date", columns="Salesman", values="Revenue")
 print(p1)
@@ -135,7 +135,7 @@ pivat_table Method
 """
 import pandas as pd
 
-foods = pd.read_csv("pandas/data/foods.csv")
+foods = pd.read_csv("data/foods.csv")
 p1 = foods.pivot_table(values="Spend", index="Gender", aggfunc="sum")
 p2 = foods.pivot_table(values="Spend", index=["Gender", "Item"], aggfunc="sum")
 p3 = foods.pivot_table(values="Spend", index=["Gender", "Item"], columns="City", aggfunc="sum")
@@ -148,7 +148,7 @@ pd.melt Method
 """
 import pandas as pd
 
-sales = pd.read_csv("pandas/data/quarters.csv")
+sales = pd.read_csv("data/quarters.csv")
 m1 = pd.melt(sales, id_vars="Salesman")
 m2 = pd.melt(sales, id_vars="Salesman", var_name="Quarter")
 m3 = pd.melt(sales, id_vars="Salesman", var_name="Quarter", value_name="Revenue")
